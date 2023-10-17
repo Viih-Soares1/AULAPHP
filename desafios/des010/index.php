aee9f7aa16a531a529dd5e80ddc8cecf60e50b7a
Vinicius <viih.soares5030@gmail.com>
2023-10-17 14:51:52 -0300

    colocando JS no desafio 10

--- desafios/des010/index.php
+++ desafios/des010/index.php
@@ -11,8 +11,8 @@
         <form method="get" action="<?= $_SERVER['PHP_SELF']; ?>">
             <label for="Valor">Digite o Valor </label>
             <input type="number" name="valor" id="id valor">
-            <label for="reajuste"> Porcentagem de Reajuste <strong><span id="p">?%</span></strong></label>
-            <input type="range" name="reajuste" min="0" max="100">
+            <label for="reajuste"> Porcentagem de Reajuste (<strong><span id="p">?</span>%</strong>)</label>
+            <input type="range" name="reajuste" min="0" max="100" step="1" oninput="mudavalor()" id="reaj">
             <input type="submit" value="Calcular">  
             
         </form> 
@@ -24,6 +24,12 @@
             echo "<p> O valor com o reajuste de $reajuste% é $porcentagem";
             ?>
         </section>  
+        <script> 
+            mudavalor();
+            function mudavalor(){
+                p.innerText = reaj.value;
+            }
+        </script>
     </main>
 </body>
 </html>
